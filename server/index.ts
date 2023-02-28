f6a9ffe9d60ee397c4c84a49693398d6394eecbf
Romango <homer37@yandex.ru>
2023-02-28 22:25:25 +0400

    начало разработки - создал create-react-app, и бек на экспрессе

---
+++ server/index.ts
@@ -0,0 +1,36 @@
+
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { json } from "body-parser";
+import cookieParser from "cookie-parser";
+import mongoose, { ConnectOptions } from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(json());
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ greeting: "Hello world!" });
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
+    app.listen(PORT, () => {
+      console.log(`🚀 server started 1at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
